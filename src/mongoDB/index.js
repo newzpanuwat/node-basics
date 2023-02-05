e14fb120b1fcf7be4f87986b917127ec8213b2f4
New Panuwat <new_sbh@me.com>
2023-02-05 20:18:53 +0700

    [MDB-23] add mongodb packages & add simple routes

---
+++ src/mongoDB/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const handlebars = require("handlebars");
+const exphbs = require("express-handlebars");
+const { allowInsecurePrototypeAcccess } = require("@handlebars/allow-prototype-access");
+const bodyparser = require("body-parser");
+
+var app = express();
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h2>Welcome to db</h2>
+    <h3>Click here go get access the <b> <a href="/student/list">Database</a></b></h3>
+  `);
+});
+
+app.listen(8000, () => {
+  console.log("server started at port 8000");
+});
